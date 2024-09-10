2afdb1a58fd26a7193f70d42c302005a1af1b182
Nela Petria <nela_petria@yahoo.com>
2024-09-10 11:54:05 +0300

    test

--- backend/api/test.js
+++ backend/api/test.js
@@ -1,3 +1,13 @@
 module.exports = (req, res) => {
-  res.status(200).json({ message: 'API is working' });
-};
+    console.log('Verbose test route accessed');
+    console.log('Request method:', req.method);
+    console.log('Request headers:', req.headers);
+    console.log('Request body:', req.body);
+  
+    res.status(200).json({
+      message: 'Verbose test route is working',
+      method: req.method,
+      headers: req.headers,
+      body: req.body
+    });
+  };
