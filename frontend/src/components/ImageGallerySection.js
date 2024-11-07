183652cd7a586a7e05fd80eb7faa48d8c9976189
Nela Petria <nela_petria@yahoo.com>
2024-11-07 12:19:58 +0200

    image gallery update

--- frontend/src/components/ImageGallerySection.js
+++ frontend/src/components/ImageGallerySection.js
@@ -9,7 +9,12 @@ function ImageGallerySection() {
     'images/3.jpg',
     'images/4.jpg',
     'images/5.jpg',
-    'images/6.jpg'
+    'images/6.jpg',
+    'images/7.jpg',
+    'images/8.jpg',
+    'images/9.jpg',
+    'images/10.jpg',
+    'images/11.jpg'
   ];
 
   return (
